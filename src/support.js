e0aece072399ae6ea5e7d29555c768aec91e277c
gokulgk2000 <>
2024-02-22 18:43:23 +0530

    package json and builder io update

--- src/support.js
+++ src/support.js
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { BuilderComponent, builder } from '@builder.io/react'
+builder.init('61ed16fdafa54924b5decd7a334091d2')
 
 const SupportPage = () => {
+    const [builderContentJson, setBuilderContentJson] = useState([])
+    useEffect(() => { 
+        builder.get('faq-1')
+          .promise().then(setBuilderContentJson)
+      }, [])
+      console.log("builderContentJson",builderContentJson)
   return (
-    <div>support</div>
+   <div className='mt-5'><p>support</p> <BuilderComponent model="faq-1" content={builderContentJson} /></div>
   )
 }
 
